108b8fd89694c00924653bee3589f91bfd740a64
al-robertson <al_robertson@sky.com>
2023-04-29 23:56:03 +0100

    Has bug with: Cannot read properties of undefined reading map

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
 const { Configuration, OpenAIApi } = require("openai");
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
 const configuration = new Configuration({
     organization: "org-45RfObFrTImEAcWt6V9IgX5d",
-    apiKey: "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",
+    apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
-//callApi()
-
 //create a simple API which calls the function above
 
-const express = require('express')
-const app = express()
-const port = 3080
+ const app = express()
+ app.use(bodyParser.json())
+ app.use(cors())
+ const port = 3080
 
-app.post('/', async (req, res) => {
+ app.post('/', async (req, res) => {
+    const {message, currentModel} = req.body;
+    console.log(message)
     const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "Say this is a test",
-        max_tokens: 7,
-        temperature: 0,
-      });
-      console.log(response.data.choices[0].text)
-      res.json({
-        data: response.data
-      })
+         model: `${currentModel}`,//"text-davinci-003",
+         prompt: `${message}`,S
+         max_tokens: 100,
+         temperature: 0.5,
+       });
+       res.json({
+         message: response.data.choices[0].text,
+       })
+ });
+
+ app.get('/models', async (req, res) => {
+    const response = await openai.listEngines();
+    console.log(response.data.data)
+    res.json({
+      models: response.data.data
+    })
 });
-app.listen(port, () => {
-    console.log(`App is listening at http://localhost:${port}`)
-});
+ 
+
+ app.listen(port, () => {
+     console.log(`App is listening at http://localhost:${port}`)
+ });
 
